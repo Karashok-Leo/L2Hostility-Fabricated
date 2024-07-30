8d0646fccf2a052db4068c740d9ab19029eaaae5
Karashok-Leo <1010177289@qq.com>
2024-07-30 17:05:03 +0800

    Fix FlameThorn cause infinite effect through recursion

--- src/main/java/karashokleo/l2hostility/content/item/trinket/misc/FlamingThorn.java
+++ src/main/java/karashokleo/l2hostility/content/item/trinket/misc/FlamingThorn.java
@@ -3,8 +3,9 @@ package karashokleo.l2hostility.content.item.trinket.misc;
 import io.github.fabricators_of_create.porting_lib.entity.events.living.LivingHurtEvent;
 import karashokleo.l2hostility.content.item.trinket.core.DamageListenerTrinketItem;
 import karashokleo.l2hostility.init.LHConfig;
-import karashokleo.l2hostility.init.LHTexts;
+import karashokleo.l2hostility.init.LHDamageTypes;
 import karashokleo.l2hostility.init.LHEffects;
+import karashokleo.l2hostility.init.LHTexts;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
@@ -26,6 +27,7 @@ public class FlamingThorn extends DamageListenerTrinketItem
     @Override
     public void onHurting(ItemStack stack, LivingEntity entity, LivingHurtEvent event)
     {
+        if (event.getSource().isOf(LHDamageTypes.SOUL_FLAME)) return;
         int size = event.getEntity().getActiveStatusEffects().size();
         if (size == 0) return;
         event.getEntity().addStatusEffect(new StatusEffectInstance(LHEffects.FLAME, LHConfig.common().items.flameThornTime, size - 1), entity);
